4575a8b22e4944649ccdcde47768afbc797d0513
Angel Peña <jose.angpc@gmail.com>
2020-01-14 22:46:19 -0600

    First version of the project

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,14 @@
+const task3Element = document.getElementById('task-3');
+
+function hi(){
+    alert("Hello everyone");
+    const task3Element = document.getElementById('task-3');
+}
+
+hi();
+
+function hifi(name){
+     alert('Hi ' + name);
+}
+
+hifi('Angel');
